91bd367747840d54fc85d818cbfe070ceb75af81
moisesJurad0 <moises003@gmail.com>
2017-04-18 11:17:55 -0500

    [COMMIT] pruebasUnitarias.ejemplo

---
+++ pruebasUnitarias.ejemplo/servicio/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, null);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(servicio.Class1.ejemplo2());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(servicio.Class1.ejemplo3());
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(servicio.Class1.ejemplo4(true));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsTrue(servicio.Class1.ejemplo5(true));
+        }
+    }
+}
---
+++ pruebasUnitarias.ejemplo/servicio/servicio/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace servicio
+{
+    public class Class1
+    {
+        public static void ejemplo1()
+        {
+            var x = 0m;
+        }
+
+        public static bool ejemplo2()
+        {
+            return true;
+        }
+
+        public static bool ejemplo3()
+        {
+            return false;
+        }
+
+        public static bool ejemplo4(bool x)
+        {
+            return x;
+        }
+
+        public static bool ejemplo5(bool x)
+        {
+            return !x;
+        }
+    }
+}
